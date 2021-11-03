0eeb8c2eb47e3b6c58744ca7883b7e0566e63199
YanHau <h97881992@gmail.com>
2021-11-04 02:48:03 +0800

    add Message Type

--- lib/types.ts
+++ lib/types.ts
@@ -4,6 +4,14 @@ export type EventCallback<T> = (data: T, ...args: any[]) => void;
 
 export type SocketEvent = string;
 
+/* Text chat */
+export type Message = {
+  roomId: string;
+  message: string;
+  who: string;
+  at: number;
+};
+
 export type ConnectRequest = { sdp: RTCSessionDescription, etag: string };
 
 export type ConnectContext = { pc: RTCPeerConnection, audio: HTMLAudioElement };
