36ed5cf0d0d9018d0e552630e49d1850bfcf3a63
YanHau <h97881992@gmail.com>
2021-11-13 21:13:46 +0800

    Express static 路徑

--- serve.js
+++ serve.js
@@ -11,8 +11,8 @@ node.stdout.on('data', data => console.log(Buffer.from(data).toString('utf-8')))
 node.stderr.on('data', data => console.log(Buffer.from(data).toString('utf-8')));
 
 app.use("/", express.static('example'))
+  .use("/", express.static('release'))
   .use("/", express.static('release/lib'))
-  .use("/lib", express.static('lib'))
   .listen(PORT, '0.0.0.0', () => {
     console.log(`example1@ http://localhost:${PORT}/text.html`);
     console.log(`example2@ http://localhost:${PORT}/video.html`);
