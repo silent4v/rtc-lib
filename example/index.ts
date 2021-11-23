04e1c7af334aa7c4fbbd969687c51d49f7077b29
yanhau <yhchen.space@gmail.com>
2021-11-23 14:24:34 +0800

    註解

--- example/index.ts
+++ example/index.ts
@@ -7,18 +7,21 @@ import { clientInit } from "./utils/client.js";
 import { serverInit } from "./utils/server.js";
 
 const PORT = 30000;
+
+/* RESTful Application */
 const httpServer = express();
 
-httpServer.use(express.json());
+httpServer
+  .use(express.json())
+  .get("/health", (_, res) => {
+    const runTime = (performance.now() / 1000) | 0;
+    let h = (runTime / 3600) | 0;
+    let m = ((runTime % 3600) / 60) | 0;
+    let s = (runTime % 60) | 0;
+    res.status(200).end(`running: ${h} hr ${m} min ${s} sec`);
+  });
 
-httpServer.get("/health", (_, res) => {
-  const runTime = (performance.now() / 1000) | 0;
-  let h = (runTime / 3600) | 0;
-  let m = ((runTime % 3600) / 60) | 0;
-  let s = (runTime % 60) | 0;
-  res.status(200).end(`running: ${h} hr ${m} min ${s} sec`);
-})
-
+/* WebSocket Handler */
 const sockServer = serverInit(new WebSocketServer({ noServer: true }));
 sockServer.on("connection", rawSock => {
   const client = clientInit(rawSock);
@@ -26,6 +29,7 @@ sockServer.on("connection", rawSock => {
   regMessengerEvent(sockServer, client);
 });
 
+/* Raw Http server */
 createServer(httpServer)
   .on("upgrade", (request, socket, head) => {
     sockServer.handleUpgrade(request, socket as any, head, function done(ws) {
