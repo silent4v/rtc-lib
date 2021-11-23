07ff5895f3f639ca96aec5735c2de89ee95ff090
yanhau <yhchen.space@gmail.com>
2021-11-23 14:23:32 +0800

    支援 ESM unit-test

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ export default {
   preset: 'ts-jest',
   verbose: true,
   testEnvironment: 'node',
-  testRegex: ".*\\.(test|spec)\\.tsx?$"
+  testRegex: ".*\\.(test|spec)\\.tsx?$",
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
