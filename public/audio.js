84a02d79c5941e0e74e1ea23937bcf7cb324be6d
YanHau <yhchen.space@gmail.com>
2021-12-15 19:39:45 +0800

    Add Call

--- public/audio.js
+++ public/audio.js
@@ -13,5 +13,8 @@ targetRoom.forEach(radio => {
 connectBtn.onclick = async () => {
     await navigator.mediaDevices.getUserMedia({ audio: true }).then(RTC.setDevice);
     const [room] = await RTC.request("room::list", currentRoom);
-    room.clients.filter(sid => sid !== RTC.sessionId).forEach(sid => console.log(sid));
+    room.clients.filter(sid => sid !== RTC.sessionId).forEach(sid => {
+      console.log("Call To:", sid.slice(0,7));
+      RTC.call(sid);
+    });
   };
